29357cc8fb04c1a39390979c627f69a4d5b7830d
cosm0s <apascualco@gmail.com>
2016-10-24 20:01:15 +0200

    Add unit test example

---
+++ test/UnittestExample.py
@@ -0,0 +1,14 @@
+import unittest
+
+class UnittestExample(unittest.TestCase):
+    def setUp(self):
+        print 'Join to setUp'
+        self.list = [0,1,5,8]
+
+    def tearDown(self):
+        print 'Join to tear down'
+
+    def test_01(self):
+        print 'Join to test_01'
+        ls = [0,1,5,8]
+	self.assertEqual(ls, self.list)
