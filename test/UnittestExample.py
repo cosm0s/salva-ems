1a7dd0cd6095bbcc186a91e302251c78b21fb660
cosm0s <apascualco@gmail.com>
2016-10-24 20:12:35 +0200

    Fix version

--- test/UnittestExample.py
+++ test/UnittestExample.py
@@ -2,13 +2,13 @@ import unittest
 
 class UnittestExample(unittest.TestCase):
     def setUp(self):
-        print 'Join to setUp'
+        print('Join to setUp')
         self.list = [0,1,5,8]
 
     def tearDown(self):
-        print 'Join to tear down'
+        print('Join to tear down')
 
     def test_01(self):
-        print 'Join to test_01'
+        print('Join to test_01')
         ls = [0,1,5,8]
 	self.assertEqual(ls, self.list)
